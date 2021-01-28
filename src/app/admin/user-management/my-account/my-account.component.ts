de0e2671f6977f7cbc4d98a8d932bfa0b75859f4
Fayçal Jebali <faycal.jebali1@gmail.com>
2021-01-28 10:55:42 +0100

    nettoyage de code

--- src/app/admin/user-management/my-account/my-account.component.ts
+++ src/app/admin/user-management/my-account/my-account.component.ts
@@ -1,23 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../../services/user.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { UserService } from "../../../services/user.service";
 
 @Component({
-  selector: 'app-my-account',
-  templateUrl: './my-account.component.html',
-  styleUrls: ['./my-account.component.scss']
+  selector: "app-my-account",
+  templateUrl: "./my-account.component.html",
+  styleUrls: ["./my-account.component.scss"],
 })
 export class MyAccountComponent implements OnInit {
-  current_user: any;
-  constructor(
-    private userService: UserService,
-  ) { 
-    this.current_user = JSON.parse(localStorage.getItem('current_user'));    
-    console.log('current_user :: ', this.current_user);
-  }
+  newUserForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private userService: UserService) {}
 
   ngOnInit() {
-    // localStorage.getItem('current_user')
-    // this.userService.getCurrentUser()
+    this.newUserForm = this.fb.group({
+      fullName: null,
+    });
   }
 
+  /**
+   * Ajouter un utilisateur
+   */
+  newUser() {
+    if (this.newUserForm.valid) {
+      console.log("this.formationForm.value : ", this.newUserForm.value);
+      this.userService.newUser(this.newUserForm.value).subscribe(
+        (result) => {
+          console.log("new User OK : ", result);
+        },
+        (err) => {
+          console.log("new User Error :", err);
+        }
+      );
+    }
+  }
+
+  /**
+   * After a form is initialized, we link it to our main form
+   */
+  formInitialized(name: string, form: FormGroup) {
+    this.newUserForm.setControl(name, form);
+  }
 }
